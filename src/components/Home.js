69d2f19734890a3800081049e5ed2650e584169c
Hans Vos <hans.vos@codeimmersives.com>
2020-04-19 15:14:11 -0400

    added bootstarp, with navbar

---
+++ src/components/Home.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import Header from './Header';
+
+class Home extends Component {
+  render(){
+    return (
+     
+      <div>
+        Home page
+      </div>
+    );
+  }
+}
+
+export default Home;
