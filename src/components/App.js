f323dc20b99fe1a3fea4194157ab6850d6de5847
Hans Vos <hans.vos@codeimmersives.com>
2020-04-20 15:52:48 -0400

    working to get backend to frontend to connect

---
+++ src/components/App.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+// import Song from './Song'
+// import Header from './Header';
+
+class Home extends Component {
+  constructor(){
+    super()
+    this.state = {
+      songs: [],
+      song: {},
+      toggle: true,
+    }
+  }
+
+  getSongs = () => {
+    const url = '/journals';
+    axios.get(url).then((song) => {
+      console.log('song', song)
+      console.log('songs data, ', song.data)
+      return this.setState( {songs: song.data})
+    })
+  }
+
+  handleNewJournalSubmit = (event, song) => {
+    event.preventDefault();
+    let axiosConfig = {
+      headers:{
+        'Content-Type' : 'application/json;charset=UTF-8',
+        'Access-Control-Allow-Origin' : '*',
+      },
+    };
+    axios.post('/song', song, axiosConfig).then(() => {
+      this.getSongs();
+    })
+  };
+
+  onDelete = (id) => {
+    axios.delete(`/song/${id}`).then(() => {
+      this.getSongs();
+    })
+  }
+
+  onUpdate = (id) => {
+    console.log('This song will be updated')
+  }
+ 
+  render(){
+    return (
+      <div>
+        <h1>music app</h1>
+      </div>
+      
+    );
+  }
+}
+
+export default Home;
